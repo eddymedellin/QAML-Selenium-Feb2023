1f86f3da5dfe8bed6b90f8a3e329b9a6a85d288b
Jennifer Ramirez <jxr20920@dexcom.com>
2023-05-08 20:26:02 -0700

    Init commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+
+public class BaseTest {
+    private WebDriver webDriver;
+
+    public WebDriver getWebDriver() {
+        File ruta = new File("/Users/jxr20920/Downloads/geckodriver-2");
+        System.setProperty("webdriver.gecko.driver", ruta.getPath());
+        //System.setProperty("webdriver.chrome.driver", ruta.getPath());
+        webDriver = new FirefoxDriver();
+        return webDriver;
+    }
+}
