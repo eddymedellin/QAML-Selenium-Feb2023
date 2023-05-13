cd06ceb9521353fb52066577c396589322bc06fc
memosky3 <memo_inge@hotmail.com>
2023-05-13 13:50:34 -0600

    change basetest to original
    change basetest to original

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,7 +7,7 @@ public class BaseTest {
     private WebDriver webDriver;
 
     public WebDriver getWebDriver() {
-        File ruta = new File("C:\\FireFoxDriver");
+        File ruta = new File("/Users/jxr20920/Downloads/geckodriver-2r");
         System.setProperty("webDriver.gecko.driver", ruta.getPath());
         //System.setProperty("webdriver.chrome.driver", ruta.getPath());
         webDriver = new FirefoxDriver();
