08b27ac6399ee7bf0faa4bcdb176f447f89adf24
Jennifer Ramirez <jxr20920@dexcom.com>
2023-05-15 09:25:11 -0700

    scroll to element JavaScript executor

--- src/main/java/Steps/BaseSteps.java
+++ src/main/java/Steps/BaseSteps.java
@@ -1,6 +1,8 @@
 package Steps;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class BaseSteps {
     public WebDriver webDriver;
@@ -32,5 +34,9 @@ public class BaseSteps {
 
     public void imprimir(String cadenaAImprimir) {
         System.out.println(cadenaAImprimir);
+    }
+
+    public void scrollToElementJS(WebElement element) {
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView();", element);
     }
 }
