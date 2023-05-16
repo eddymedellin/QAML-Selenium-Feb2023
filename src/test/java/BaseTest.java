f9c34c8d71d1693fb45bbe1f8c3020694679d04a
Abraham Miranda <abraham.miranda.rodriguez@gmail.com>
2023-05-15 21:39:28 -0600

    Practica 5 primer push

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,7 +7,8 @@ public class BaseTest {
     private WebDriver webDriver;
 
     public WebDriver getWebDriver() {
-        File ruta = new File("/Users/jxr20920/Downloads/geckodriver-2");
+        //File ruta = new File("/Users/jxr20920/Downloads/geckodriver-2");
+        File ruta = new File("/Users/abe/Documents/geckodriver-v0.33.0-macos-aarch64/geckodriver");
         System.setProperty("webdriver.gecko.driver", ruta.getPath());
         //System.setProperty("webdriver.chrome.driver", ruta.getPath());
         webDriver = new FirefoxDriver();
