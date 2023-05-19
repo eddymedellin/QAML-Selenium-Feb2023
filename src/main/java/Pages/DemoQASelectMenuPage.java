c7e8de3d1ed7d4d09086cae7034a32d6f5144281
yes1307 <yeseniaheraz07@gmail.com>
2023-05-18 20:37:10 -0600

    Revert "TareaCategorias3"
    This reverts commit 169b4752e4325695f0d555827556ddc25ad4ed11.

---
+++ src/main/java/Pages/DemoQASelectMenuPage.java
@@ -0,0 +1,27 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DemoQASelectMenuPage extends  BasePage{
+    public DemoQASelectMenuPage(WebDriver _webdriver) {
+        super(_webdriver);
+    }
+
+    private WebElement setListaColores(){
+        return webDriver.findElement(By.id("oldSelectMenu"));
+    }
+
+    public WebElement getListaColores(){
+        return setListaColores();
+    }
+
+    private WebElement setListaCarros(){
+        return webDriver.findElement(By.id("cars"));
+    }
+    public WebElement getListaCarros(){
+        return setListaCarros();
+    }
+
+}
