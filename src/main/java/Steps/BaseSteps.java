532357e05fac5133d250537e66beaf5c624886bd
Jennifer Ramirez <jxr20920@dexcom.com>
2023-05-18 19:14:23 -0700

    Actions Drag and Drop

--- src/main/java/Steps/BaseSteps.java
+++ src/main/java/Steps/BaseSteps.java
@@ -1,6 +1,9 @@
 package Steps;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class BaseSteps {
     public WebDriver webDriver;
@@ -32,5 +35,42 @@ public class BaseSteps {
 
     public void imprimir(String cadenaAImprimir) {
         System.out.println(cadenaAImprimir);
+    }
+
+    public void scrollToElementJS(WebElement element) {
+        //No es necesario maximizar la ventana
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    public void maximizarVentana() {
+        webDriver.manage().window().maximize();
+    }
+
+    public void scrollToElement(WebElement element) {
+        new Actions(webDriver)
+                .scrollToElement(element)
+                .perform();// <- Luz verde para ejecutar la/las acciones
+    }
+
+    public void moveToElementClickDragDrop(WebElement elementDroppable,
+                                           WebElement drag) {
+        int y = elementDroppable.getLocation().y;
+        int x = elementDroppable.getLocation().x;
+        int sizey = elementDroppable.getSize().height;
+        int sizex = elementDroppable.getSize().width;
+
+        int coordx = x + sizex/2;
+        int coordy = y + sizey/2;
+
+        new Actions(webDriver)
+                .dragAndDropBy(drag, coordx, coordy)
+                .perform();
+    }
+
+    public void dragAndDropElement(WebElement elementDrop,
+                                   WebElement drag) {
+        new Actions(webDriver)
+                .dragAndDrop(drag, elementDrop)
+                .perform();
     }
 }
