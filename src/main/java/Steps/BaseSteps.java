3abc88aec95b85287e00c903be434bcd206b285b
Jennifer Ramirez <jxr20920@dexcom.com>
2023-05-18 19:14:23 -0700

    Actions Drag and Drop

--- src/main/java/Steps/BaseSteps.java
+++ src/main/java/Steps/BaseSteps.java
@@ -1,7 +1,7 @@
 package Steps;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
